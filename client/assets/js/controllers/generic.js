492b5d2cd4083f4c6bae18a486e128204c1534a9
Marco Arras <Marco Arras>
2016-03-16 13:10:09 -0500

    new species template and navigation for index

--- client/assets/js/controllers/generic.js
+++ client/assets/js/controllers/generic.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var genericController = function ($scope, $state, $http, multiple, single){
 
     // Grab URL parameters
--- client/assets/js/controllers/index.js
+++ client/assets/js/controllers/index.js
@@ -5,4 +5,16 @@ var genericController = require('./generic');
 angular.module('application')
 	.controller('FilmsCtrl', function($scope, $state, $http){
 	    $scope = genericController($scope, $state, $http, 'films', 'film');
-	}); 
+	})
+	.controller('SpeciesCtrl', function($scope, $state, $http){
+		$scope = genericController($scope, $state, $http, 'species', 'specie');
+	})
+	.controller('PlanetsCtrl', function($scope, $state, $http){
+		$scope = genericController($scope, $state, $http, 'planets', 'planet');
+	})
+	.controller('PeopleCtrl', function($scope, $state, $http){
+		$scope = genericController($scope, $state, $http, 'people', 'person');
+	})
+	.controller('StarshipsCtrl', function($scope, $state, $http){
+		$scope = genericController($scope, $state, $http, 'starships', 'starship');
+	});
